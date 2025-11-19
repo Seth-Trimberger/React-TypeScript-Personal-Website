35c6c63cdeb85c7aca534d0ba3f57b9a54c5d210
Seth Trimberger <sethtrimberger@Seths-MacBook-Pro.local>
2025-11-18 23:58:49 -0800

    Stared work on my websitesss

--- src/Components/MainPage/MainPage.tsx
+++ src/Components/MainPage/MainPage.tsx
@@ -3,8 +3,12 @@ import "./MainPage.css"
 function MainPage(){
     return(
         <>
+            <div>Hello! I am Seth Im a current junior at </div>
             <div>Seth Trimberger</div>
             <div>509 491 0852</div>
+                        <div>5ethyyyy</div>
+
+
             <div></div>
         </>
     );
